eac83a86d88185358b143a07a21f94bbafacc2f8
Vedanty5501 <Vedanty5501@gmail.com>
2025-07-08 23:56:10 +0530

    Port error fixed

--- frontend/src/axios.js
+++ frontend/src/axios.js
@@ -3,5 +3,5 @@ import axios from "axios";
 window.axios = axios
 axios.defaults.withCredentials = false
 // axios.defaults.baseURL = "http://localhost:8000/api"
-let backendUrl = "http://" + window.location.hostname.toString() + ":8001/api"
+let backendUrl = "http://" + window.location.hostname.toString() + ":"+process.env.PORT+"/api"
 axios.defaults.baseURL = backendUrl
