9433783db03eb33bd5db385669b1f8c96d10d63b
Vedanty5501 <Vedanty5501@gmail.com>
2025-07-09 00:18:34 +0530

    fix4

--- frontend/src/axios.js
+++ frontend/src/axios.js
@@ -4,4 +4,5 @@ window.axios = axios
 axios.defaults.withCredentials = false
 // axios.defaults.baseURL = "http://localhost:8000/api"
 let backendUrl = "http://" + window.location.hostname.toString() + ":8080/api"
+console.log("Backend URL: ", backendUrl)
 axios.defaults.baseURL = backendUrl
