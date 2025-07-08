0b29adccc6b86c7a94049f45bd1ef7c56ec5f430
Vedanty5501 <94383127+Vedanty5501@users.noreply.github.com>
2025-07-08 17:19:55 +0530

    Update database.js

--- backend/config/database.js
+++ backend/config/database.js
@@ -3,10 +3,11 @@ import mysql from "mysql2";
 // create the connection to database
 
 const db = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "root",
-    database: "db_restaurant"
+    host: "process.env.DB_HOST",
+    user: "process.env.DB_USER",
+    password: "process.env.DB_PASSWORD",
+    database: "process.env.DB_NAME",
+    port: process.env.DB_PORT
 });
 
 
